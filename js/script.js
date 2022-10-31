a898557462192868fba8e75aca32e9cb84d9ad75
marcofcp <marco_fcp85@hotmail.com>
2022-10-31 18:44:35 +0000

    fixed position on header and page paddings for links

--- js/script.js
+++ js/script.js
@@ -29,8 +29,6 @@ const navboxHidden = document.querySelector('.nav-box-hidden');
 window.addEventListener("scroll", (evt) => {
     if (window.scrollY > 60) {
         header.style.backgroundColor = "#DCC79C";
-        header.style.position = "fixed";
-        // header.style.zIndex = "9";
         header.style.padding = "1em 0em 1em 0em";
         logoName.style.color = "#2B1104";
         hamburger.style.color = "#2B1104";
